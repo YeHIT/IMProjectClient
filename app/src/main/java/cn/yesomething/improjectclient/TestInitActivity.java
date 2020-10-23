22965945b921ce449b969f4582b6f4d42f4c8416
YeHIT <1044377312@qq.com>
2020-10-23 21:40:29 +0800

    feature(manager,chat):初步搭建了会话列表的获取与更新。 date:2020/10/23 21:45 author:Ye

--- app/src/main/java/cn/yesomething/improjectclient/TestInitActivity.java
+++ app/src/main/java/cn/yesomething/improjectclient/TestInitActivity.java
@@ -5,9 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.tencent.imsdk.v2.V2TIMConversation;
+import com.tencent.imsdk.v2.V2TIMConversationListener;
+import com.tencent.imsdk.v2.V2TIMConversationResult;
+import com.tencent.imsdk.v2.V2TIMManager;
 import com.tencent.imsdk.v2.V2TIMUserFullInfo;
 import com.tencent.imsdk.v2.V2TIMValueCallback;
 
+import java.util.Date;
 import java.util.List;
 
 import cn.yesomething.improjectclient.chat.ChatActivity;
@@ -23,9 +28,13 @@ public class TestInitActivity extends Activity {
         setContentView(R.layout.activity_test_init);
         //todo 暂放
         IMManager.initSDKConfig(this);
+
         Log.e(TAG, "onCreate: "+IMManager.isLoginIMService());
         if(IMManager.isLoginIMService()){
             showLoginUser();
+            //初始化会话监听器
+            initConversationListener();
+            Log.e(TAG, "onCreate: 配置会话监听器成功！" );
             // 跳转到聊天界面
             Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
             startActivity(intent);
@@ -38,6 +47,36 @@ public class TestInitActivity extends Activity {
         }
     }
 
+
+    private void initConversationListener(){
+        V2TIMManager.getConversationManager().setConversationListener(new V2TIMConversationListener() {
+            @Override
+            public void onNewConversation(List<V2TIMConversation> conversationList) {
+                Log.e(TAG, new Date()+"onNewConversation: 收到新会话" );
+            }
+
+            @Override
+            public void onConversationChanged(List<V2TIMConversation> conversationList) {
+                Log.e(TAG, new Date()+"onConversationChanged: 会话改变了");
+            }
+        });
+        Log.e(TAG, "initConversationListener: 初始化会话监听器成功" );
+        V2TIMManager.getConversationManager().getConversationList(0, 50,
+                new V2TIMValueCallback<V2TIMConversationResult>() {
+                    @Override
+                    public void onError(int code, String desc) {
+                        // 拉取会话列表失败
+                    }
+                    @Override
+                    public void onSuccess(V2TIMConversationResult v2TIMConversationResult) {
+                        Log.e(TAG, "onSuccess: 拉取会话列表成功！" );
+                    }
+        });
+    }
+
+    /**
+     * 展示当前登录的用户信息
+     */
     private void showLoginUser(){
         //查看已登录用户的信息
         UserManager.getUserInfo(IMManager.getLoginUser(),new V2TIMValueCallback<List<V2TIMUserFullInfo>>(){
