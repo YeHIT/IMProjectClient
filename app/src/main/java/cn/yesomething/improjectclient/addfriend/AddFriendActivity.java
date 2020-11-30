84e1d651ad45e30635c7211301136c57bbf83566
denwade-05 <59132981+denwade-05@users.noreply.github.com>
2020-11-30 19:39:36 +0800

    dgb修改addFriendActivity的提示Toast

--- app/src/main/java/cn/yesomething/improjectclient/addfriend/AddFriendActivity.java
+++ app/src/main/java/cn/yesomething/improjectclient/addfriend/AddFriendActivity.java
@@ -1,24 +1,36 @@
 package cn.yesomething.improjectclient.addfriend;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.yesomething.improjectclient.R;
 import cn.yesomething.improjectclient.manager.FriendsManager;
+import cn.yesomething.improjectclient.manager.MyServerManager;
 
 
 public class AddFriendActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView addUserId;
     private TextView addUserDes;
+    boolean isFriend;
     @BindView(R.id.contact_new_back)
     Button _btnBack;
 
@@ -52,13 +64,15 @@ public class AddFriendActivity extends AppCompatActivity implements View.OnClick
             case R.id.add_friend_send:{
                 String contentID = addUserId.getText().toString();
                 String contentDes = addUserDes.getText().toString();
-                if(!"".equals(contentID)){
+                /*if(!"".equals(contentID)){
                     Toast.makeText(this,contentID,Toast.LENGTH_SHORT).show();
                     FriendsManager.addFriend(contentID,contentDes);
                 }
                 else{
                     Toast.makeText(this,"Empty!",Toast.LENGTH_SHORT).show();
                 }
+                break;*/
+                testUserAddFriend(contentID,contentDes);
                 break;
 
             }
@@ -66,5 +80,91 @@ public class AddFriendActivity extends AppCompatActivity implements View.OnClick
                 break;
             }
         }
+    }
+
+    public void testUserAddFriend(String userName,String contentDes){
+        //一开始时记得声明handler
+        Handler userSelectHandler = null;
+        //todo 输入要查询用户的用户名
+//        String userName = "xx";
+        //用于获取最终的数据并展示
+        userSelectHandler = new Handler(Looper.myLooper(),new Handler.Callback(){
+            @Override
+            public boolean handleMessage(@NonNull Message msg) {
+                String response = (msg != null) ? (String) msg.obj : null;
+                //网络正常
+                if(response != null){
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String responseCode = jsonObject.getString("responseCode");
+                        //正常获取到数据
+                        if(responseCode.equals("200")){
+                            jsonObject = jsonObject.getJSONObject("user");
+                            //todo 利用数据展示
+                            testFriendSelect(userName,contentDes);
+                            //Toast.makeText(AddFriendActivity.this ,userName,Toast.LENGTH_SHORT).show();
+                            //FriendsManager.addFriend(userName,contentDes);
+
+
+                        }
+                        else if(responseCode.equals("503")){
+                            Toast.makeText(AddFriendActivity.this ,"没有此用户"+userName,Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            //todo 错误信息处理
+                            String errorMessage = jsonObject.getString("errorMessage");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                //todo 网络异常判断
+                else {
+                }
+                return false;
+            }
+        });
+        MyServerManager.userSelect(userSelectHandler,userName);
+    }
+
+    //好友关系查询
+    public void testFriendSelect(String friendName,String contentDes){
+        //一开始时记得声明handler
+        Handler selectFriendHandler = null;
+
+        //用于获取最终的数据并展示
+        selectFriendHandler = new Handler(Looper.myLooper(),new Handler.Callback(){
+            @Override
+            public boolean handleMessage(@NonNull Message msg) {
+                String response = (msg != null) ? (String) msg.obj : null;
+                //网络正常
+                if(response != null){
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String responseCode = jsonObject.getString("responseCode");
+                        //todo 说明存在好友关系
+                        if(responseCode.equals("200")){
+                            Toast.makeText(AddFriendActivity.this ,friendName+"已经为你的好友",Toast.LENGTH_SHORT).show();
+                        }
+                        //不存在好友关系
+                        else if (responseCode.equals("510")) {
+                            Toast.makeText(AddFriendActivity.this ,"添加好友"+friendName,Toast.LENGTH_SHORT).show();
+                            FriendsManager.addFriend(friendName,contentDes);
+                        }
+                        else{
+                            //todo 错误信息处理
+                            String errorMessage = jsonObject.getString("errorMessage");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                //todo 网络异常判断
+                else {
+                }
+                return false;
+            }
+        });
+        MyServerManager.userSelect(selectFriendHandler,friendName);
     }
 }
